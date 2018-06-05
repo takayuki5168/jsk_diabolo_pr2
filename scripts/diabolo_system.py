0158f1e5d691e29a56823ca803ee3ca4622e3f55
Takayuki Murooka <takayuki5168@gmail.com>
2018-06-05 17:52:27 +0900

    add publish_diabolo_model.cpp

--- scripts/diabolo_system.py
+++ scripts/diabolo_system.py
@@ -10,14 +10,26 @@
 # increase log_files
 
 # CHECK
-# self.init_state is [0, 0]
+# (self.init_state is [0, 0]   # only use for simulation)
 # self.state_ref is [0, 0]
 # action is 2
+# self.MAX_INPUT_DIFF_RESTRICTION
 
 # TODOTODOTODO
 # armの符号が逆、だけどなぜかpitchは収束する...
+#   NNの中間層の数を変えると治る
+# sometime, subscribe is very slow
 
-import random, time
+
+# consider frequency of noise
+#  apply lowpassfilter
+
+# consider covariance of observation
+
+# if not using zerograds, what happens
+
+
+import random, time, sys
 import numpy as np
 
 import chainer
@@ -31,6 +43,7 @@ from matplotlib import pyplot as plt
 import rospy
 from std_msgs.msg import Float64MultiArray, Float64
 
+'''
 LOG_FILES = ['../log/log-by-logger/log-by-loggerpy0.log',
              '../log/log-by-logger/log-by-loggerpy1.log',
              '../log/log-by-logger/log-by-loggerpy2.log',
@@ -38,16 +51,21 @@ LOG_FILES = ['../log/log-by-logger/log-by-loggerpy0.log',
              '../log/log-by-logger/log-by-loggerpy4.log',
              '../log/log-by-logger/log-by-loggerpy5.log',
              '../log/log-by-logger/log-by-loggerpy6.log']             
+'''
+LOG_FILES = ['../log/log-by-logger/log-by-loggerpy1_0.log',
+             '../log/log-by-logger/log-by-loggerpy1_1.log',
+             '../log/log-by-logger/log-by-loggerpy1_2.log',
+             '../log/log-by-logger/log-by-loggerpy1_3.log']
 
 class MyChain(Chain):
     def __init__(self):
         super(MyChain, self).__init__(   # FIX
-            l1=L.Linear(6, 1),
+            l1=L.Linear(6, 2),
             l2=L.Linear(2)
             )
         
     def forward(self, x):   # FIX
-        h = F.relu(self.l1(x))
+        h = F.sigmoid(self.l1(x))
         o = self.l2(h)
         return o
 
@@ -57,54 +75,74 @@ class MyChain(Chain):
 
     def loss(self, t):
         return F.mean_squared_error(self.res, t)
+    
+    ''' loss for optimize_input()
+    def loss(self, t):
+        return F.mean_squared_error(self.res, t)
+    '''
 
 class DiaboloSystem():
     def __init__(self):
-        self.input_arm = []
-        self.input_base = []
-        self.state_pitch = []
-        self.state_yaw = []
-
+        # PAST NUM
         self.PAST_STATE_NUM = 2   # FIX
         self.PAST_INPUT_NUM = 1   # FIX
         self.PAST_NUM = max(self.PAST_STATE_NUM, self.PAST_INPUT_NUM)
+
+        # INPUT DIM
         self.STATE_DIM = 2
         self.INPUT_DIM = 2
         
         self.DELTA_STEP = 5   # FIX
 
+        # HyperParameters for NeuralNetwork
         self.INPUT_NN_DIM = self.STATE_DIM * self.PAST_STATE_NUM + self.INPUT_DIM * self.PAST_INPUT_NUM
         self.OUTPUT_NN_DIM = self.STATE_DIM
-        
-        self.VAR_NUM = max(self.INPUT_NN_DIM, self.OUTPUT_NN_DIM)
-
-        self.state_ref = [0., 0.]   # FIX
-        #self.state_ref = [10., 0.]   # FIX        
-
+        #self.VAR_NUM = max(self.INPUT_NN_DIM, self.OUTPUT_NN_DIM)
         self.train_test_ratio = 0.8   # FIX
         self.batch_size = 1000   # FIX
 
-        #self.init_state = [-20., 0.]
-        self.init_state = [-20., 0.]        
+        # reference of state
+        self.state_ref = [0., 0.]   # FIX
+        #self.state_ref = [10., 0.]   # FIX        
+
+        # real data
         self.past_states = []
         self.now_input = [0.7, 0]
-        
-        self.MAX_INPUT_DIFF_RESTRICTION = [0.03, 0.001]
+
+        # max min restriction for input
+        #self.MAX_INPUT_DIFF_RESTRICTION = [0.03, 0.001]
+        self.MAX_INPUT_DIFF_RESTRICTION = [10, 10]   # TODOTODOTODO
         self.MAX_INPUT_RESTRICTION = [0.85, 0.34]   # TODO
         self.MIN_INPUT_RESTRICTION = [0.60, -0.34]   # TODO        
-        
+
+        # init ros variable
         rospy.init_node("DiaboloSystem")
         self.pub_input = rospy.Publisher('/diabolo_system/diabolo_input', Float64MultiArray, queue_size=1)
 
+    # print percentage of progress every 10%
     def percentage(self, idx, loop_num):
         split_num = 10
         if (idx + 1) % int(loop_num / split_num) == 0:
             print('{}% {}/{}'.format((idx + 1) * 100 / loop_num, idx + 1, loop_num))
-        
+
+    # load data from log files
     def load_data(self, log_files):
+        self.input_arm = []
+        self.input_base = []
+        self.state_pitch = []
+        self.state_yaw = []
+
+        lpf_average_num = 10
+        self.input_arm_lpf = []
+        self.input_base_lpf = []
+        self.state_pitch_lpf = []
+        self.state_yaw_lpf = []
+        
         for log_file in log_files:
             with open(log_file, "r") as f:
+                cnt = 0
                 for l in f.readlines():
+                    cnt += 1
                     val = l.split(' ')
                     if len(val) != 4:
                         break
@@ -114,10 +152,38 @@ class DiaboloSystem():
                     self.state_pitch.append(float(val[2]))
                     self.state_yaw.append(float(val[3][:-1]))
 
+                    if cnt > lpf_average_num:
+                        a = 0
+                        b = 0
+                        p = 0
+                        y = 0
+                        for i in range(lpf_average_num):
+                            a += self.input_arm[-i]
+                            b += self.input_base[-i]
+                            p += self.state_pitch[-i]
+                            y += self.state_yaw[-i]
+                        self.input_arm_lpf.append(a / lpf_average_num)
+                        self.input_base_lpf.append(b / lpf_average_num)
+                        self.state_pitch_lpf.append(p / lpf_average_num)
+                        self.state_yaw_lpf.append(y / lpf_average_num)
+
+    # arrange data for NeuralNetwork
     def arrange_data(self):
         self.X = []
         self.Y = []
         
+        for i in range((max(self.PAST_STATE_NUM, self.PAST_INPUT_NUM) + 1) * self.DELTA_STEP, len(self.input_arm_lpf)):
+            x = []
+            y = [self.state_pitch_lpf[i], self.state_yaw_lpf[i]]            
+            for j in range(self.PAST_STATE_NUM):
+                x.append(self.state_pitch_lpf[i - (j + 1) * self.DELTA_STEP])
+                x.append(self.state_yaw_lpf[i - (j + 1) * self.DELTA_STEP])
+            for j in range(self.PAST_INPUT_NUM):
+                x.append(self.input_arm_lpf[i - (j + 1) * self.DELTA_STEP])
+                x.append(self.input_base_lpf[i - (j + 1) * self.DELTA_STEP])
+            self.X.append(x)
+            self.Y.append(y)
+        '''
         for i in range((max(self.PAST_STATE_NUM, self.PAST_INPUT_NUM) + 1) * self.DELTA_STEP, len(self.input_arm)):
             x = []
             y = [self.state_pitch[i], self.state_yaw[i]]            
@@ -129,7 +195,8 @@ class DiaboloSystem():
                 x.append(self.input_base[i - (j + 1) * self.DELTA_STEP])
             self.X.append(x)
             self.Y.append(y)
-
+        '''         
+    # make NeuralNetwork model by using MyChain class
     def make_model(self):
         self.model = MyChain()
         self.optimizer = optimizers.RMSprop(lr=0.01)
@@ -152,6 +219,12 @@ class DiaboloSystem():
             x.append(self.X[s + i])
             y.append(self.Y[s + i])
         return np.array(x), np.array(y)
+    
+    def load_model(self, log_file='../log/diabolo_system/mymodel.h5'):
+        serializers.load_hdf5(log_file, self.model)
+        
+    def save_model(self):
+        serializers.save_hdf5('../log/diabolo_system/mymodel.h5', self.model)
         
     def train(self, loop_num=1000, plot_loss=True):
         print('[DebugPrint] Training Start')        
@@ -178,12 +251,6 @@ class DiaboloSystem():
             plt.yscale('log')
             plt.show()
             
-    def save_model(self):
-        serializers.save_hdf5('../log/diabolo_system/mymodel.h5', self.model)
-
-    def load_model(self, log_file='../log/diabolo_system/mymodel.h5'):
-        serializers.load_hdf5(log_file, self.model)
-        
     def test(self):
         x, y = self.get_test()        
         x_ = Variable(x.astype(np.float32).reshape(len(x),6))
@@ -201,27 +268,27 @@ class DiaboloSystem():
         print(loss.data)
 
     def realtime_feedback(self):
-        rospy.Subscriber("calc_idle_diabolo_state/diabolo_state", Float64MultiArray, self.callback_for_state)
+        rospy.Subscriber("calc_idle_diabolo_state/diabolo_state", Float64MultiArray, self.callback_for_state, queue_size=1)
         rospy.spin()        
-        # callback_for_state
+        # callback_for_state is working background
         #   optimize_input
         #   publish_input
-
-    def publish_input(self):
-        msg = Float64MultiArray()
-        msg.data = [self.now_input[0], self.now_input[1]]
-        self.pub_input.publish(msg)
 
     def callback_for_state(self, msg):
         if msg.data[0] == np.nan:
             msg.data[0] = self.past_states[-1][0]
         if msg.data[1] == np.nan:
-            msg.data[1] = self.past_states[-1][1]            
+            msg.data[1] = self.past_states[-1][1]
         self.past_states.append([msg.data[0], msg.data[1]])
-        if len(self.past_states) > self.PAST_STATE_NUM * self.DELTA_STEP:        
+        if len(self.past_states) > self.PAST_STATE_NUM * self.DELTA_STEP:
             self.optimize_input()
             print('{} {} {} {}'.format(self.now_input[0], self.now_input[1], self.past_states[-1][0], self.past_states[-1][1]))
             self.publish_input()                            
+
+    def publish_input(self):
+        msg = Float64MultiArray()
+        msg.data = [self.now_input[0], self.now_input[1]]
+        self.pub_input.publish(msg)
 
     # CHECK
     def optimize_input(self): # TODO add max input restriction
@@ -259,7 +326,10 @@ class DiaboloSystem():
         self.now_input = [float(x[0][self.PAST_STATE_NUM * self.STATE_DIM + 0].data), float(x[0][self.PAST_STATE_NUM * self.STATE_DIM + 1].data)]
 
     def simulate(self, simulate_loop_num=1000):
-        self.past_states = [self.init_state for i in range(10)]
+        #self.init_state = [40., 0.]
+        self.init_state = [40., 0.]        
+        
+        self.past_states = [self.init_state for i in range(self.PAST_STATE_NUM * self.DELTA_STEP)]
         print('[DebugPrint] Simulation Start')        
         with open('../log/diabolo_system/simulate.log', 'w') as f:
             for i in range(simulate_loop_num):   # simulation loop
@@ -277,7 +347,10 @@ class DiaboloSystem():
 
 if __name__ == '__main__':
     train_flag = False
-    action = 2  # 0:test  1:simulate  2:realtime feedback
+    if len(sys.argv) == 2:
+        action = int(sys.argv[1])
+    else:
+        action = 2  # 0:test  1:simulate  2:realtime feedback
     
     ds = DiaboloSystem()
 
@@ -286,13 +359,13 @@ if __name__ == '__main__':
         ds.load_data(LOG_FILES)
         ds.arrange_data()
         ds.make_model()
-        ds.train(loop_num=1000, plot_loss=False)
+        ds.train(loop_num=1000, plot_loss=True)
         ds.save_model()
     else:
         ds.load_data(LOG_FILES)
         ds.arrange_data()
         ds.make_model()    
-        ds.load_model(log_file='../log/diabolo_system/goodmodel2.h5')
+        ds.load_model(log_file='../log/diabolo_system/mymodel.h5')
     
     if action == 0:   # test
         ds.test()
---
+++ src/publish_diabolo_model.cpp
@@ -0,0 +1,80 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <visualization_msgs/Marker.h>
+
+class PublishDiaboloModelNode
+{
+public:
+  PublishDiaboloModelNode() : nh_(""), pnh_("~"), r_(30)
+  {
+    // Subscriber
+    sub_diabolo_state_ = pnh_.subscribe("/calc_idle_diabolo_state/diabolo_state", 1, &PublishDiaboloModelNode::messageCallback, this);
+
+    // Publisher
+    pub_marker_diabolo_model_ = pnh_.advertise<visualization_msgs::Marker>("diabolo_model", 1);
+
+    // Marker
+    initMarker();
+  }
+
+  void execute()
+  {
+    while (true) {
+      ros::spinOnce();
+      publishMarker();
+    }
+  }
+  
+private:
+  void initMarker() {
+    /*
+     * marker state
+     */
+    marker_diabolo_model_.header.frame_id = "/base_footprint";
+    marker_diabolo_model_.header.stamp = ros::Time::now();
+    marker_diabolo_model_.ns = "diabolo_model_marker";
+    marker_diabolo_model_.id = 0;
+    marker_diabolo_model_.mesh_resource = "package://jsk_diabolo_pr2/meshes/diabolo.dae";
+    marker_diabolo_model_.action = visualization_msgs::Marker::ADD;
+    marker_diabolo_model_.scale.x = 1;
+    marker_diabolo_model_.scale.y = 1;
+    marker_diabolo_model_.scale.z = 1;
+    marker_diabolo_model_.color.r = 0.0f;
+    marker_diabolo_model_.color.g = 0.8f;
+    marker_diabolo_model_.color.b = 0.7f;
+    marker_diabolo_model_.color.a = 0.8;
+    marker_diabolo_model_.lifetime = ros::Duration();
+  }
+
+  void publishMarker()
+  {
+    pub_marker_diabolo_model_.publish(marker_diabolo_model_);
+  }
+  
+  void messageCallback(const std_msgs::Float64MultiArray diabolo_state)
+  {
+    pitch_ = diabolo_state.data[0];
+    yaw_ = diabolo_state.data[1];
+  }
+
+  // ros params
+  ros::NodeHandle nh_, pnh_;
+  ros::Subscriber sub_diabolo_state_;
+  ros::Publisher pub_marker_diabolo_model_; // Marker
+  visualization_msgs::Marker marker_diabolo_model_;
+  ros::Rate r_;
+
+  // diabolo state
+  double pitch_, yaw_;  
+};
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "publish_diabolo_model");
+
+  PublishDiaboloModelNode n;
+  n.execute();
+
+  return 0;
+}
