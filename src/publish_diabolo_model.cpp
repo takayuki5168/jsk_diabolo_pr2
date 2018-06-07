305b64b0a081761ddd3727976476d82325af2537
Takayuki Murooka <takayuki5168@gmail.com>
2018-06-08 07:49:45 +0900

    po

--- src/publish_diabolo_model.cpp
+++ src/publish_diabolo_model.cpp
@@ -1,7 +1,8 @@
 #include <ros/ros.h>
 #include <std_msgs/Float64MultiArray.h>
 #include <visualization_msgs/Marker.h>
-#include "tf/transform_broadcaster.h"
+//#include "tf/transform_broadcaster.h"
+#include <tf/transform_listener.h>
 
 class PublishDiaboloModelNode
 {
@@ -15,13 +16,27 @@ public:
     // Publisher
     pub_marker_diabolo_model_ = pnh_.advertise<visualization_msgs::Marker>("diabolo_model", 1);
     pub_marker_string_right_ = pnh_.advertise<visualization_msgs::Marker>("diabolo_string_right", 1);
-    pub_marker_string_left_ = pnh_.advertise<visualization_msgs::Marker>("diabolo_string_left", 1);        
+    pub_marker_string_left_ = pnh_.advertise<visualization_msgs::Marker>("diabolo_string_left", 1);
 
     // Marker
     initMarker();
   }
 
-  void execute() { ros::spin(); }
+  void execute() {
+    while (true) {
+      try {
+	tfl_right_.lookupTransform("/base_footprint", "/r_gripper_r_finger_tip_link", ros::Time(0), tf_right_);
+	tfl_left_.lookupTransform("/base_footprint", "/l_gripper_l_finger_tip_link", ros::Time(0), tf_left_);
+      }
+      catch (tf::TransformException ex){
+	//ROS_ERROR("%s",ex.what());
+      }
+      std::cout << tf_right_.getOrigin().x() << " " << tf_right_.getOrigin().y() << " " << tf_right_.getOrigin().z() << std::endl;
+      std::cout << tf_left_.getOrigin().x() << " " << tf_left_.getOrigin().y() << " " << tf_left_.getOrigin().z() << std::endl;      
+      
+      ros::spinOnce();
+    }
+  }
   
 private:
   void initMarker() {
@@ -53,9 +68,10 @@ private:
     marker_string_right_.id = 0;
     marker_string_right_.type = visualization_msgs::Marker::LINE_STRIP;    
     marker_string_right_.action = visualization_msgs::Marker::ADD;
-    marker_string_right_.scale.x = 1;
-    marker_string_right_.scale.y = 1;
-    marker_string_right_.scale.z = 1;
+    marker_string_right_.scale.x = 10;
+    marker_string_right_.scale.y = 10;
+    marker_string_right_.scale.z = 10;    
+    marker_string_right_.pose.orientation.w = 1.0;
     marker_string_right_.color.r = 1.0f;
     marker_string_right_.color.g = 1.0f;
     marker_string_right_.color.b = 1.0f;
@@ -68,12 +84,13 @@ private:
     marker_string_left_.header.stamp = ros::Time::now();
     marker_string_left_.ns = "diabolo_string_left";
     marker_string_left_.id = 0;
-    marker_string_left_.type = visualization_msgs::Marker::LINE_STRIP;    
+    marker_string_left_.type = visualization_msgs::Marker::LINE_STRIP;
     marker_string_left_.action = visualization_msgs::Marker::ADD;
-    marker_string_left_.scale.x = 1;
-    marker_string_left_.scale.y = 1;
-    marker_string_left_.scale.z = 1;
-    marker_string_left_.color.r = 1.0f;
+    marker_string_left_.scale.x = 10;
+    marker_string_left_.scale.y = 10;
+    marker_string_left_.scale.z = 10;    
+    marker_string_left_.pose.orientation.w = 1.0;    
+    marker_string_left_.color.r = 0.0f;
     marker_string_left_.color.g = 1.0f;
     marker_string_left_.color.b = 1.0f;
     marker_string_left_.lifetime = ros::Duration();
@@ -93,25 +110,54 @@ private:
     quaternionTFToMsg(q_diabolo, marker_diabolo_model_.pose.orientation);
     pub_marker_diabolo_model_.publish(marker_diabolo_model_);
 
+    geometry_msgs::Point p;
+    
     /*
      * marker string right
      */
     marker_string_right_.header.frame_id = "/base_footprint";
     marker_string_right_.header.stamp = ros::Time::now();
-    //marker_string_right_.position.x = 1;
-    //marker_string_right_.position.y = 1;
-    //marker_string_right_.position.z = 1;
-    // pub_marker_string_right_.publish(marker_string_right_);
+    marker_string_right_.points.clear();
+    marker_string_right_.pose.orientation.x = 0.0;
+    marker_string_right_.pose.orientation.y = 0.0;
+    marker_string_right_.pose.orientation.z = 0.0;
+    marker_string_right_.pose.orientation.w = 1.0;
+    marker_string_right_.pose.position.x = 0.0;
+    marker_string_right_.pose.position.y = 0.0;
+    marker_string_right_.pose.position.z = 0.0;
+    p.x = 100;//tf_right_.getOrigin().x();
+    p.y = 100;//tf_right_.getOrigin().y();
+    p.z = 100;//tf_right_.getOrigin().z();     
+    marker_string_right_.points.push_back(p);
+    p.x = 0.7;
+    p.y = 0;
+    p.z = 0.21;
+    marker_string_right_.points.push_back(p);
+    p.x = 100;
+    p.y = 0;
+    p.z = 0;
+    marker_string_right_.points.push_back(p);    
+    pub_marker_string_right_.publish(marker_string_right_);
 
     /*
      * marker string left
      */
     marker_string_left_.header.frame_id = "/base_footprint";
     marker_string_left_.header.stamp = ros::Time::now();
-    //marker_string_left_.position.x = 1;
-    //marker_string_left_.position.y = 1;
-    //marker_string_left_.position.z = 1;
-    // pub_marker_string_left_.publish(marker_string_left_);
+    marker_string_left_.points.clear();    
+    p.x = tf_left_.getOrigin().x();
+    p.y = tf_left_.getOrigin().y();
+    p.z = tf_left_.getOrigin().z();     
+    marker_string_left_.points.push_back(p);
+    p.x = 0.7;
+    p.y = 0;
+    p.z = 0.21;
+    marker_string_left_.points.push_back(p);
+    p.x = 100;
+    p.y = 0;
+    p.z = 0;
+    marker_string_left_.points.push_back(p);    
+    pub_marker_string_left_.publish(marker_string_left_);
   }
   
   void messageCallbackForDiaboloState(const std_msgs::Float64MultiArray diabolo_state)
@@ -128,6 +174,8 @@ private:
   ros::Publisher pub_marker_diabolo_model_, pub_marker_string_right_, pub_marker_string_left_;   // Marker
   visualization_msgs::Marker marker_diabolo_model_, marker_string_right_, marker_string_left_;
   ros::Rate r_;
+  tf::TransformListener tfl_right_, tfl_left_;
+  tf::StampedTransform tf_right_, tf_left_;  
 
   // diabolo state
   double pitch_, yaw_;  
