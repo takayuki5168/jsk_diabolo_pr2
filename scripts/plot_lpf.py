07282764c62845f841b8017c758641231848164c
Takayuki Murooka <takayuki5168@gmail.com>
2019-01-14 14:12:52 +0900

    modify constructure with using scripts, scripts_node and scripts_plot

---
+++ scripts/plot_lpf.py
@@ -0,0 +1,20 @@
+from matplotlib_starter import MatplotlibStarter, MetaData
+import sys, signal
+
+signal.signal(signal.SIGINT, lambda signal, frame: sys.exit(0))
+
+ax1 = [MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log_with_robust.log", [-1, 2], label="Pitch", color="blue", lambda_function=[lambda x:x/100., lambda x:x], start=33, end=48),
+       MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log_with_robust.log", [-1, 3], label="Yaw", color="green", lambda_function=[lambda x:x/100., lambda x:x], start=32, end=48)]
+ax2 = [MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log_with_robust.log", [-1, 0], label="u1", color="purple", lambda_function=[lambda x:x/100., lambda x:x-0.7], start=32, end=48),
+       MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log_with_robust.log", [-1, 1], label="u2", color="orange", lambda_function=[lambda x:x/100., lambda x:x*20], start=32, end=48)]
+
+ax1_title = ["Pitch Angle of Diabolo", "time[s]", "angle[degree]"]
+ax2_title = ["Yaw Angle of Diabolo", "time[s]", ""]
+
+plotter = MatplotlibStarter(hspace=0.6)
+plotter.execute(2, 1,
+                [[ax1],
+                 [ax2]],
+                [[ax1_title],
+                 [ax2_title]])
+
