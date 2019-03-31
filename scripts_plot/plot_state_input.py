cc67964092b3c52982ccf005203f1325b1918d89
Takayuki Murooka <takayuki5168@gmail.com>
2019-03-31 19:41:30 +0900

    update graphs

--- scripts_plot/plot_state_input.py
+++ scripts_plot/plot_state_input.py
@@ -8,8 +8,8 @@ ax1 = [MetaData("log/diabolo_state_input.log", [-1, 2], label="Pitch", color="bl
 ax2 = [MetaData("log/diabolo_state_input.log", [-1, 0], label="u1", color="purple", lambda_function=[lambda x:x/100., lambda x:-x+0.7]),
        MetaData("log/diabolo_state_input.log", [-1, 1], label="u2", color="orange", lambda_function=[lambda x:x/100., lambda x:-x*20])]
 
-ax1_title = ["State of Diabolo", "time[s]", "angle[degree]"]
-ax2_title = ["Control Input from Robot", "time[s]", ""]
+ax1_title = ["Attitude of Diabolo", "time[s]", "angle[degree]"]
+ax2_title = ["Control Input of Robot", "time[s]", ""]
 
 plotter = MatplotlibStarter(hspace=0.6)
 plotter.execute(2, 1,
--- scripts_plot/plot_state_input_robust.py
+++ scripts_plot/plot_state_input_robust.py
@@ -8,8 +8,8 @@ ax1 = [MetaData("log/diabolo_state_input_with_robust.log", [-1, 2], label="Pitch
 ax2 = [MetaData("log/diabolo_state_input_with_robust.log", [-1, 0], label="u1", color="purple", lambda_function=[lambda x:x/100., lambda x:-x+0.7], start=36, end=48),
        MetaData("log/diabolo_state_input_with_robust.log", [-1, 1], label="u2", color="orange", lambda_function=[lambda x:x/100., lambda x:-x*20], start=36, end=48)]
 
-ax1_title = ["State of Diabolo", "time[s]", "angle[degree]"]
-ax2_title = ["Control Input from Robot", "time[s]", ""]
+ax1_title = ["Attitude of Diabolo", "time[s]", "angle[degree]"]
+ax2_title = ["Control Input of Robot", "time[s]", ""]
 
 plotter = MatplotlibStarter(hspace=0.6)
 plotter.execute(2, 1,
